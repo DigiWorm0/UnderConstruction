4a28bf179bd222bb46201b2f9dbb8c553f391348
DigiWorm <aus343@gmail.com>
2022-05-10 00:27:24 -0500

    Built Page using React

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src={"/logo128.png"}
+                    className="App-logo"
+                    alt="logo" />
+
+                <h3 className="App-text">
+                    We are working on LevelImposter v2!
+                </h3>
+
+                <h6 className="App-text">
+                    Come check our progress on our <a
+                        className="App-link"
+                        href="https://discord.gg/9jJEBQ4qJs"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Discord
+                    </a> server.
+                </h6>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
